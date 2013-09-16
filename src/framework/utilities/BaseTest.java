d2012975ecde01a7d15aeacf292f8c1c70439e3d
nvonop <nvonop@gmail.com>
2013-09-16 17:39:55 +0100

    Updated BaseTest to remove instantiation of System Properties within class
    System Properties should now be passed in through a build system such as Gradle.

--- src/framework/utilities/BaseTest.java
+++ src/framework/utilities/BaseTest.java
@@ -17,26 +17,26 @@ import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
 
-	public WebDriver driver = null;
-	protected static String applicationUrl = System.getProperty(
-			"APPLICATION_URL", "http://www.google.com/");
-	protected static String browser = System.getProperty("BROWSER", "firefox");
-	protected static String version = System.getProperty("VERSION", "19");
-	protected static String platform = System.getProperty("PLATFORM", "MAC");
-	protected static boolean localDriver = Boolean.valueOf(System.getProperty(
-			"LOCAL_DRIVER", "true"));
-	protected static boolean remoteDriver = Boolean.valueOf(System
-			.getProperty("REMOTE_DRIVER", "false"));
-	protected static boolean sauceDriver = Boolean.valueOf(System.getProperty(
-			"SAUCE_DRIVER", "false"));
-	protected static String gridHost = System.getProperty("GRID_HOST",
-			"localhost");
-	protected static int gridPort = Integer.valueOf(System.getProperty(
-			"GRID_PORT", "4444"));
-	protected static String sauceKey = System.getProperty("SAUCE_KEY",
-			"http://USER:TOKEN@ondemand.saucelabs.com:80/wd/hub");
-	protected static String testResultsPath = System.getProperty(
-			"TEST_RESULTS_PATH", "~");
+	public ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+//	protected static String applicationUrl = System.getProperty(
+//			"APPLICATION_URL", "http://www.google.com/");
+//	protected static String browser = System.getProperty("BROWSER", "firefox");
+//	protected static String version = System.getProperty("VERSION", "19");
+//	protected static String platform = System.getProperty("PLATFORM", "MAC");
+//	protected static boolean localDriver = Boolean.valueOf(System.getProperty(
+//			"LOCAL_DRIVER", "true"));
+//	protected static boolean remoteDriver = Boolean.valueOf(System
+//			.getProperty("REMOTE_DRIVER", "false"));
+//	protected static boolean sauceDriver = Boolean.valueOf(System.getProperty(
+//			"SAUCE_DRIVER", "false"));
+//	protected static String gridHost = System.getProperty("GRID_HOST",
+//			"localhost");
+//	protected static int gridPort = Integer.valueOf(System.getProperty(
+//			"GRID_PORT", "4444"));
+//	protected static String sauceKey = System.getProperty("SAUCE_KEY",
+//			"http://USER:TOKEN@ondemand.saucelabs.com:80/wd/hub");
+//	protected static String testResultsPath = System.getProperty(
+//			"TEST_RESULTS_PATH", "~");
 
 	@BeforeClass
 	public void beforeClass() {
@@ -48,7 +48,8 @@ public class BaseTest {
 	}
 
 	protected void closeWebdriver() {
-		driver.quit();
+		driver.get().quit();
+		driver.set(null);
 	}
 
 	public void setLocalWebdriver(String browser, String version,
@@ -61,16 +62,16 @@ public class BaseTest {
 		case 0:
 			throw new WebDriverException("Browser: " + browser + " not found.");
 		case 1:
-			driver = new InternetExplorerDriver(capabilities);
+			driver.set(new InternetExplorerDriver(capabilities));
 			break;
 		case 2:
-			driver = new FirefoxDriver(capabilities);
+			driver.set(new FirefoxDriver(capabilities));
 			break;
 		case 3:
-			driver = new SafariDriver(capabilities);
+			driver.set(new SafariDriver(capabilities));
 			break;
 		case 4:
-			driver = new ChromeDriver(capabilities);
+			driver.set(new ChromeDriver(capabilities));
 			break;
 		}
 	}
@@ -115,7 +116,7 @@ public class BaseTest {
 		capabilities.setCapability("platform", platform);
 
 		try {
-			this.driver = new RemoteWebDriver(new URL(sauceKey), capabilities);
+			this.driver.set(new RemoteWebDriver(new URL(System.getProperty("SAUCE_KEY")), capabilities));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
@@ -148,25 +149,25 @@ public class BaseTest {
 		capabilities.setCapability("platform", platform);
 
 		try {
-			this.driver = new RemoteWebDriver(new URL("http://" + gridHost
-					+ ":" + gridPort + "/wd/hub"), capabilities);
+			this.driver.set(new RemoteWebDriver(new URL("http://" + System.getProperty("GRID_HOST")
+					+ ":" + System.getProperty("GRID_PORT") + "/wd/hub"), capabilities));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void setWebDriver(String browser, String version, String platform) {
-		if (localDriver) {
+		if (Boolean.valueOf(System.getProperty("LOCAL_DRIVER"))) {
 			setLocalWebdriver(browser, version, platform);
-		} else if (remoteDriver) {
+		} else if (Boolean.valueOf(System.getProperty("REMOTE_DRIVER"))) {
 			setRemoteWebdriver(browser, version, platform);
-		} else if (sauceDriver) {
+		} else if (Boolean.valueOf(System.getProperty("SAUCE_DRIVER"))) {
 			setSauceWebdriver(browser, version, platform);
 		} else {
 			throw new WebDriverException("Type of driver not specified!!!");
 		}
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
+		driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get().manage().window().maximize();
 	}
 
 }
