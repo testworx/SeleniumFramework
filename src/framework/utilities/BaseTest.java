11ce647c77ae749d3196b506dee0e1c3b18374fe
nvonop <nvonop@gmail.com>
2013-09-16 22:11:56 +0100

    Changes made to update JavaDoc
    JavaDoc still a WIP!

--- src/framework/utilities/BaseTest.java
+++ src/framework/utilities/BaseTest.java
@@ -15,8 +15,16 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+/**
+ * @author nvonop
+ *
+ */
 public class BaseTest {
 
+	/**
+	 * A ThreadLocal instance of a WebDriver object.  
+	 * It should be thread safe and enable tests to run in parallel.
+	 */
 	public ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 //	protected static String applicationUrl = System.getProperty(
 //			"APPLICATION_URL", "http://www.google.com/");
@@ -24,9 +32,9 @@ public class BaseTest {
 //	protected static String version = System.getProperty("VERSION", "19");
 //	protected static String platform = System.getProperty("PLATFORM", "MAC");
 //	protected static boolean localDriver = Boolean.valueOf(System.getProperty(
-//			"LOCAL_DRIVER", "true"));
+//			"LOCAL_DRIVER", "false"));
 //	protected static boolean remoteDriver = Boolean.valueOf(System
-//			.getProperty("REMOTE_DRIVER", "false"));
+//			.getProperty("REMOTE_DRIVER", "true"));
 //	protected static boolean sauceDriver = Boolean.valueOf(System.getProperty(
 //			"SAUCE_DRIVER", "false"));
 //	protected static String gridHost = System.getProperty("GRID_HOST",
@@ -37,21 +45,42 @@ public class BaseTest {
 //			"http://USER:TOKEN@ondemand.saucelabs.com:80/wd/hub");
 //	protected static String testResultsPath = System.getProperty(
 //			"TEST_RESULTS_PATH", "~");
+	
+
 
 	@BeforeClass
-	public void beforeClass() {
-
+	protected void beforeClass() {
+//		System.setProperty("LOCAL_DRIVER", "false");
+//		System.setProperty("REMOTE_DRIVER", "true");
+//		System.setProperty("SAUCE_DRIVER", "false");
+//		
+//		System.setProperty("BROWSER", "firefox");
+//		System.setProperty("VERSION", "19");
+//		System.setProperty("PLATFORM", "MAC");
+//		
+//		System.setProperty("GRID_HOST", "localhost");
+//		System.setProperty("GRID_PORT", "4444");
 	}
 
 	@AfterClass
 	protected void afterClass() {
 	}
 
-	protected void closeWebdriver() {
+	/**
+	 * This method closes the browser associated with the ThreadLocal WebDriver object.
+	 * It also the WebDriver instance for the ThreadLocal value to null.
+	 */
+	public void closeWebdriver() {
 		driver.get().quit();
 		driver.set(null);
 	}
 
+	/**
+	 * This method initialises the ThreadLocal WebDriver object according to the supplied parameters to enable tests to run on a users local machine.
+	 * @param browser This can be set to "chrome", "internet explorer" or "firefox"
+	 * @param version This should be set to reflect the browser required.  It is only used when matching capabilities for a RemoteWebDriver object however.
+	 * @param platform This should be set to whatever platform the test is required to run on e.g. "WINDOWS", "MAC", "LINUX".  It is only used when matching capabilities for a RemoteWebDriver object however.
+	 */
 	public void setLocalWebdriver(String browser, String version,
 			String platform) {
 		
@@ -88,9 +117,14 @@ public class BaseTest {
 		} else {
 			return 0;
 		}
-
 	}
 
+	/**
+	 * This method initialises the ThreadLocal WebDriver object according to the supplied parameters to enable tests to run on Sauce Labs cloud infrastructure.
+	 * @param browser This can be set to "chrome", "internet explorer" or "firefox"
+	 * @param version This should be set to reflect the browser required.  It is only used when matching capabilities for a RemoteWebDriver object however.
+	 * @param platform This should be set to whatever platform the test is required to run on e.g. "WINDOWS", "MAC", "LINUX".  It is only used when matching capabilities for a RemoteWebDriver object however.
+	 */
 	public void setSauceWebdriver(String browser, String version,
 			String platform) {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -122,6 +156,12 @@ public class BaseTest {
 		}
 	}
 
+	/**
+	 * This method initialises the ThreadLocal WebDriver object according to the supplied parameters to enable tests to run remotely on a grid of test machines.
+	 * @param browser This can be set to "chrome", "internet explorer" or "firefox"
+	 * @param version This should be set to reflect the browser required.  It is only used when matching capabilities for a RemoteWebDriver object however.
+	 * @param platform This should be set to whatever platform the test is required to run on e.g. "WINDOWS", "MAC", "LINUX".  It is only used when matching capabilities for a RemoteWebDriver object however.
+	 */
 	public void setRemoteWebdriver(String browser, String version,
 			String platform) {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -156,6 +196,14 @@ public class BaseTest {
 		}
 	}
 
+	/**
+	 * This method provides a way of initialising a ThreadLocal WebDriver object in a consistent way regardless of where the test will run.
+	 * The following system properties must be set to "true" or "false" for this method to function correctly:  LOCAL_DRIVER, REMOTE_DRIVER, SAUCE_DRIVER.
+	 * Setting one of the properties to "true" will ensure the ThreadLocal WebDriver object is initialised appropriately.
+	 * @param browser This can be set to "chrome", "internet explorer" or "firefox"
+	 * @param version This should be set to reflect the browser required.  It is only used when matching capabilities for a RemoteWebDriver object however.
+	 * @param platform This should be set to whatever platform the test is required to run on e.g. "WINDOWS", "MAC", "LINUX".  It is only used when matching capabilities for a RemoteWebDriver object however.
+	 */
 	public void setWebDriver(String browser, String version, String platform) {
 		if (Boolean.valueOf(System.getProperty("LOCAL_DRIVER"))) {
 			setLocalWebdriver(browser, version, platform);
@@ -166,8 +214,8 @@ public class BaseTest {
 		} else {
 			throw new WebDriverException("Type of driver not specified!!!");
 		}
-		driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get().manage().window().maximize();
+		this.driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		this.driver.get().manage().window().maximize();
 	}
 
 }
