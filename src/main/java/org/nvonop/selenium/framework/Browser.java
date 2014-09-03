bca99b70489aaa397d19291263986185523110bf
nvonop <nvonop@gmail.com>
2014-09-03 23:07:53 +0100

    Changed where DesiredCapabilities are instantiated
    DesiredCapabilities are now set at class level

--- src/main/java/org/nvonop/selenium/framework/Browser.java
+++ src/main/java/org/nvonop/selenium/framework/Browser.java
@@ -285,7 +285,6 @@ public class Browser {
 	 * 
 	 */
 	private void setSauceWebdriver() {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
 
 		switch (getBrowserId(browser)) {
 		case 0:
